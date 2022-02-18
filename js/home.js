a181dae38a036778750f9d7fbd34f3187624afdc
Siegfred Samson <siegfredsamson@gmail.com>
2022-02-18 17:47:16 +0100

    JS scrollY level adjusted

--- js/home.js
+++ js/home.js
@@ -1,7 +1,7 @@
 // navbar
 let navbar = document.querySelector('.navbar');
 window.addEventListener('scroll', ()=>{
-    if(scrollY >= 270){
+    if(scrollY >= 600){
         navbar.classList.add('newBg')
     }else{
         navbar.classList.remove('newBg')
