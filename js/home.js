77c961b12968ae42cc11e5ab3905a8a8ac30f150
Siegfred Samson <siegfredsamson@gmail.com>
2022-02-18 18:05:45 +0100

    JS collage image on mouseover functions added

--- js/home.js
+++ js/home.js
@@ -8,3 +8,22 @@ window.addEventListener('scroll', ()=>{
     }
     console.log(scrollY)
 })
+
+// image collapse
+let collageImages = [...document.querySelectorAll('.collage-img')]
+
+collageImages.map((item, i)=>{
+    item.addEventListener('mouseover', ()=>{
+        collageImages.map((image, index)=>{
+            if(index !=i){
+                image.style.filter = 'blur(10px)';
+                item.style.zIndex = 2;
+            }
+        })
+    })
+    item.addEventListener('mouseleave', ()=>{
+        collageImages.map((image, index)=>{
+            image.style=null;
+        })
+    })
+})
