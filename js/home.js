f99306f2d00432edb54d2708730386856def89f0
Siegfred Samson <siegfredsamson@gmail.com>
2022-02-18 17:43:33 +0100

    JS scroll event added for navbar item

--- js/home.js
+++ js/home.js
@@ -1,5 +1,10 @@
 // navbar
 let navbar = document.querySelector('.navbar');
 window.addEventListener('scroll', ()=>{
+    if(scrollY >= 270){
+        navbar.classList.add('newBg')
+    }else{
+        navbar.classList.remove('newBg')
+    }
     console.log(scrollY)
 })
